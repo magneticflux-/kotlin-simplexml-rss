8e6bf569e951a1d1f196115283e0ad111088075b
Mitchell Skaggs <skaggsm333@gmail.com>
2018-01-01 12:06:51 -0600

    Created abstract classes for channel and item elements

--- src/main/kotlin/com/github/magneticflux/rss/Rss.kt
+++ src/main/kotlin/com/github/magneticflux/rss/Rss.kt
@@ -60,6 +60,36 @@ interface IChannel {
     val iTunesComplete: Boolean
 }
 
+abstract class AbstractChannel(
+        override val title: String,
+        override val description: String,
+        override val link: URL,
+        override val categories: List<Category>,
+        override val copyright: String?,
+        override val docs: URL?,
+        override val language: Locale?,
+        override val webMaster: String?,
+        override val managingEditor: String?,
+        override val generator: String?,
+        override val image: Image?,
+        override val lastBuildDate: ZonedDateTime?,
+        override val pubDate: ZonedDateTime?,
+        override val ttl: Int?,
+        override val skipDays: List<DayOfWeek>,
+        override val skipHours: List<Int>,
+        override val cloud: Cloud?,
+        override val textInput: TextInput?,
+        override val items: List<Item>,
+        override val iTunesCategories: List<ITunesTopLevelCategory>,
+        override val iTunesExplicit: ITunesExplicitStatus,
+        override val iTunesSubtitle: String?,
+        override val iTunesSummary: String?,
+        override val iTunesAuthor: String?,
+        override val iTunesImage: ITunesImage?,
+        override val iTunesBlock: ITunesBlock?,
+        override val iTunesComplete: Boolean
+) : IChannel
+
 /**
  * This class represents a channel in an RSS feed.
  *
@@ -96,18 +126,42 @@ data class Channel(
         override val iTunesImage: ITunesImage?,
         override val iTunesBlock: ITunesBlock?,
         internal val _iTunesComplete: String?
-) : IChannel {
-    override val iTunesExplicit: ITunesExplicitStatus = when (_iTunesExplicit?.toLowerCase()) {
-        "yes" -> ITunesExplicitStatus.YES
-        "clean" -> ITunesExplicitStatus.CLEAN
-        else -> ITunesExplicitStatus.NONE
-    }
-
-    override val iTunesComplete: Boolean = when (_iTunesComplete?.toLowerCase()) {
-        "yes" -> true
-        else -> false
-    }
-}
+) : AbstractChannel(
+        title,
+        description,
+        link,
+        categories,
+        copyright,
+        docs,
+        language,
+        webMaster,
+        managingEditor,
+        generator,
+        image,
+        lastBuildDate,
+        pubDate,
+        ttl,
+        skipDays,
+        skipHours,
+        cloud,
+        textInput,
+        items,
+        iTunesCategories,
+        when (_iTunesExplicit?.toLowerCase()) {
+            "yes" -> ITunesExplicitStatus.YES
+            "clean" -> ITunesExplicitStatus.CLEAN
+            else -> ITunesExplicitStatus.NONE
+        },
+        iTunesSubtitle,
+        iTunesSummary,
+        iTunesAuthor,
+        iTunesImage,
+        iTunesBlock,
+        when (_iTunesComplete?.toLowerCase()) {
+            "yes" -> true
+            else -> false
+        }
+)
 
 /**
  * This class represents a category in either a [Channel] or an [Item].
@@ -221,6 +275,27 @@ interface IItem {
     val iTunesBlock: ITunesBlock?
 }
 
+abstract class AbstractItem(
+        override val title: String?,
+        override val description: String?,
+        override val link: URL?,
+        override val categories: List<Category>,
+        override val comments: URL?,
+        override val pubDate: ZonedDateTime?,
+        override val author: String?,
+        override val guid: Guid?,
+        override val enclosure: Enclosure?,
+        override val source: Source?,
+        override val iTunesCategories: List<ITunesTopLevelCategory>,
+        override val iTunesExplicit: ITunesExplicitStatus,
+        override val iTunesSubtitle: String?,
+        override val iTunesSummary: String?,
+        override val iTunesAuthor: String?,
+        override val iTunesDuration: ITunesDuration?,
+        override val iTunesImage: ITunesImage?,
+        override val iTunesBlock: ITunesBlock?
+) : IItem
+
 /**
  * This class represents an item in a [Channel].
  *
@@ -248,13 +323,29 @@ data class Item(
         override val iTunesDuration: ITunesDuration?,
         override val iTunesImage: ITunesImage?,
         override val iTunesBlock: ITunesBlock?
-) : IItem {
-    override val iTunesExplicit: ITunesExplicitStatus = when (_iTunesExplicit?.toLowerCase()) {
-        "yes" -> ITunesExplicitStatus.YES
-        "clean" -> ITunesExplicitStatus.CLEAN
-        else -> ITunesExplicitStatus.NONE
-    }
-}
+) : AbstractItem(title,
+        description,
+        link,
+        categories,
+        comments,
+        pubDate,
+        author,
+        guid,
+        enclosure,
+        source,
+        iTunesCategories,
+        when (_iTunesExplicit?.toLowerCase()) {
+            "yes" -> ITunesExplicitStatus.YES
+            "clean" -> ITunesExplicitStatus.CLEAN
+            else -> ITunesExplicitStatus.NONE
+        },
+        iTunesSubtitle,
+        iTunesSummary,
+        iTunesAuthor,
+        iTunesDuration,
+        iTunesImage,
+        iTunesBlock
+)
 
 /**
  * This class represents an enclosure in an [Item].
