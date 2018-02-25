e9fed692567d61af1c26827ed015a0fac5d47241
Mitchell Skaggs <skaggsm333@gmail.com>
2018-02-25 10:26:58 -0600

    Added extension method for writing XML to a String

--- src/test/groovy/com/github/magneticflux/rss/namespaces/standard/elements/CategoryTest.groovy
+++ src/test/groovy/com/github/magneticflux/rss/namespaces/standard/elements/CategoryTest.groovy
@@ -1,7 +1,6 @@
 package com.github.magneticflux.rss.namespaces.standard.elements
 
 import com.github.magneticflux.rss.PersisterSpecification
-import org.codehaus.groovy.runtime.StringBufferWriter
 import spock.lang.Unroll
 
 class CategoryTest extends PersisterSpecification {
@@ -10,11 +9,10 @@ class CategoryTest extends PersisterSpecification {
 	def 'category  #object read/write'() {
 
 		when: 'the object is written to XML'
-		def output = new StringBuffer()
-		persister.write(object, new StringBufferWriter(output))
+		def output = persister.write(object)
 
 		then: 'the output XML equals the given XML'
-		output.toString() == xml
+		output == xml
 
 		when: 'the XML is read to an object'
 		def input = persister.read(Category, xml)
